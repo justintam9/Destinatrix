5a1a8f634293978002c6f64c2df20a798927dcbf
essamobeid <essamobeid@cmail.carleton.ca>
2020-02-10 16:21:25 -0500

    - create RegisterActivity - Create Register basic layout (email, password, sign up button) - adding firebaseauth dependency to gradle.build

--- Destinatrix/app/src/main/java/com/project/destinatrix/MainActivity.java
+++ Destinatrix/app/src/main/java/com/project/destinatrix/MainActivity.java
@@ -3,8 +3,16 @@ package com.project.destinatrix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailId, password;
+    Button btnSignUp;
+    FirebaseAuth mFireBaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
