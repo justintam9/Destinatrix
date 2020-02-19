0aeb98e0c22d060eadbf7214c7eacd7a7c137283
justintam9 <justinn_tam@hotmail.com>
2020-02-19 16:36:55 -0500

    city UI

---
+++ Destinatrix/app/src/main/java/com/project/destinatrix/CityActivity.java
@@ -0,0 +1,37 @@
+package com.project.destinatrix;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityActivity extends AppCompatActivity {
+    List<CityData> cityList;
+    Integer[] images = {R.drawable.stock_image1,R.drawable.stock_image2,R.drawable.stock_image3,R.drawable.stock_image4,R.drawable.stock_image5};
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_list);
+
+        cityList = new ArrayList<>();
+        cityList.add(new CityData("test",getRandomImage()));
+        cityList.add(new CityData("test",getRandomImage()));
+        cityList.add(new CityData("test",getRandomImage()));
+        cityList.add(new CityData("test",getRandomImage()));
+
+        RecyclerView view = findViewById(R.id.recyclerview_city);
+        CustomCityAdapter myAdapter = new CustomCityAdapter(this,cityList);
+        view.setLayoutManager(new GridLayoutManager(this,3));
+        view.setAdapter(myAdapter);
+    }
+    public Integer getRandomImage(){
+        return images[(int)(Math.random()*(images.length+1))];
+    }
+
+}
