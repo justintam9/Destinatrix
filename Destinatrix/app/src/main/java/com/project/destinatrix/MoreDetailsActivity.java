cd462f33b6c66bbf0de7a71e83d3a63203473177
SKapal <sahilkapal@cmail.carleton.ca>
2020-03-01 23:32:58 -0500

    add close button to destination more details view

--- Destinatrix/app/src/main/java/com/project/destinatrix/MoreDetailsActivity.java
+++ Destinatrix/app/src/main/java/com/project/destinatrix/MoreDetailsActivity.java
@@ -3,6 +3,8 @@ package com.project.destinatrix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MoreDetailsActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MoreDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_details);
+
+        Button close = findViewById(R.id.more_details_close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MoreDetailsActivity.super.onBackPressed();
+            }
+        });
     }
 }
